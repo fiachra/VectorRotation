592cd6e0f4d0c915e66b2f715f017e603abe0e69
Fiachra Matthews <fiachra@galvanic.ie>
2017-07-29 22:44:34 +0100

    first tests

--- test.c
+++ test.c
@@ -2,7 +2,23 @@
 #include "VectorRotation.h"
 
 int main(){
-	printf("This is a program\n");
+
+	Vec3 objectPos;
+	objectPos.x = 15;
+	objectPos.y = 0;
+	objectPos.z = 0; 
+
+	Vec3 listenerPos;
+	listenerPos.x = 10;
+	listenerPos.y = 0;
+	listenerPos.z = 0;
+
+	Rot3 listenerRot;
+	listenerRot.roll = 0;
+	listenerRot.pitch = 0;
+	listenerRot.yaw = 90;
+
+	ConvertToListenerSpace(&objectPos, &listenerPos , &listenerRot, NULL);
 
 	return 0;
 }
